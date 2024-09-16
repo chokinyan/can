94fe784ff964b289baba1d4a315efd6bd14293ce
no name <55279869+chokinyan@users.noreply.github.com>
2024-09-16 10:58:49 +0200

    tache pro 3

--- Can/src/main.cpp
+++ Can/src/main.cpp
@@ -1,27 +1,32 @@
+#include <U8g2lib.h>
+#include <Wire.h>
 #include <Arduino.h>
 #include <ACAN2515.h>
 
-// Utilisation du MCP2515 relié par bus SPI
-// Voir dans doc les pins à utiliser en fonction du type de cible (ESP32, arduino...)
-
 #define cs_2515 5          // réglage broche CS
-#define int_2515 36          // réglage broche Interuption 
+#define int_2515 36          // réglage broche Interuption
 #define f_2515 16000000     // réglage Fréquence du quartz de la carte MCP2515 
 #define f_bus_can 250000      // réglage Vitesse du bus CAN
 
-
 ACAN2515 can2515(cs_2515, SPI, int_2515); // Déclaration de l'objet CAN utilisant le réglages des broches définis avant
-
-
-// Declaration d'un masque (param 1) qui utilise les 11 bits d'identification de message
-// et qui n'utilise pas les 2 premiers octets de data (param 2 et 3) optionnels
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 const ACAN2515Mask masque = standard2515Mask(0b11111111111, 0, 0);
 
-// Declaration procedure de traitement  filtre
 void message_1octet(const CANMessage & inMessage);
 
+class RegimeMoteur {
+    public:
+        String hex;
+        int regime;
+        CANMessage message;
+        RegimeMoteur(CANMessage message) {
+            message = message;
+            regime = (message.data[0]*256 + message.data[1]) / 8;
+            hex = String(message.data[0], HEX) + String(message.data[1], HEX);
+            
+        }
+};
 
-//réglage identifiant trame voulue
 const ACAN2515AcceptanceFilter filtres[] = {
    { standard2515Filter(0x3E1, 0, 0), message_1octet }, 
 
@@ -29,20 +34,14 @@ const ACAN2515AcceptanceFilter filtres[] = {
 
 CANMessage messageCANReception;
 
-
-void setup()
+void setup(void) 
 {
-	Serial.begin(9600);
-	Serial.println("Init CAN ok");
-	delay(500);
-	SPI.begin();
-	ACAN2515Settings can_vit(f_2515, f_bus_can);
-    
-	// Demarrage CAN (le 2eme param s'appelle "une closure" il permet de relier
-	// le code à la fonction qui traite l'intéruption dans la lib ACAN2515)
-	const uint16_t v_err = can2515.begin(can_vit, [] { can2515.isr(); },masque, filtres, 2 );   
-	
-	if (v_err == 0) 
+  Serial.begin(9600);
+  SPI.begin();
+  ACAN2515Settings can_vit(f_2515, f_bus_can);
+  const uint16_t v_err = can2515.begin(can_vit, [] { can2515.isr(); },masque, filtres, 2 );   
+  
+  if (v_err == 0) 
     { 
         Serial.println("Recepteur: Config ok");
     } 
@@ -54,15 +53,24 @@ void setup()
 	
 	Serial.println("Attente de message !");
 	delay(500);
+
+  u8g2.setI2CAddress(0x78);
+  u8g2.begin();
+  u8g2.enableUTF8Print(); //nécessaire pour écrire des caractères accentués
+
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_6x13_tf);
+  u8g2.setCursor(5,32);
+  u8g2.print("Gherbi & Lalaouna");
+  u8g2.sendBuffer();
+  delay(2000);
 }
 
 
-//Traitement message par fonction
 void message_1octet(const CANMessage & inMessage)
 {
 
 }
-
 
 void loop()
 {
@@ -71,15 +79,17 @@ void loop()
     {
     // Un message CAN est arrive 
     
-    can2515.receive(messageCANReception) ;
-  
-    Serial.println("Messages obtenus: ") ;
-    Serial.println(messageCANReception.data[0]);
-   
-    } 
-    
-    
-    delay(100);
+    can2515.receive(messageCANReception);
 
+    RegimeMoteur tourMin(messageCANReception);
+    
+    Serial.println(tourMin.regime);
+    
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_6x13_tf);
+    u8g2.setCursor(5,16);
+    u8g2.print("0 1 2 3 4 5 6 7 8");
+    u8g2.sendBuffer();
+    }
+    delay(100);
 }
-	
